65e93e0d0a1acc7338f0a20a6c0d8fdae3403a49
Oscar Soler <osolertf@gmail.com>
2026-01-23 12:59:09 -0800

    v0.1.6

--- src/flux_ratios_pipeline_funcs.py
+++ src/flux_ratios_pipeline_funcs.py
@@ -34,7 +34,7 @@ from gigalens.tf.profiles.mass import sis, shear, epl, sie
 import multiprocessing
 import time
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 print('flux_ratios_pipeline_funcs.py version:', __version__)
 
 """
